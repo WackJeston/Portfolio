d9a8c59eaa8d168bf324560f20a96ea5f3fa34f3
WackJeston <33kcaj33@gmail.com>
2021-08-17 16:35:41 +0100

    Type Writer Animation 95% Complete

---
+++ js/typing-animation.js
@@ -0,0 +1,12 @@
+new TypeIt("#typing", {
+  speed: 50,
+  waitUntilVisible: true,
+  loop: true,
+  startDelay: 500
+})
+  .type("I'm a Web Developer!", {delay: 500})
+  .delete(1)
+  .type(", in training...", {delay: 2000})
+  .delete(35, {delay: 300})
+  .type("What's for lunch?", {delay: 1000})
+  .go();
