9159ccace1b638e392547866465c3d628feafd7d
WackJeston <33kcaj33@gmail.com>
2021-12-01 15:55:05 +0000

    JS Components Complete

--- js/image-view.js
+++ js/image-view.js
@@ -1,21 +1,52 @@
-const mainImage = document.querySelector(".main-img")
+const desktopImage1 = document.querySelector(".desk-1")
+const mobileImage1 = document.querySelector(".mobile-1")
+const desktopImage2 = document.querySelector(".desk-2")
+const mobileImage2 = document.querySelector(".mobile-2")
+const desktopImage3 = document.querySelector(".desk-3")
+const mobileImage3 = document.querySelector(".mobile-3")
 const overlay = document.querySelector(".overlay")
 const largeImage = document.querySelector(".large-img")
 const body = document.querySelector("body")
 
 let viewSwitch = false
 
-mainImage.addEventListener("click", function(){
+desktopImage1.addEventListener("click", function(){
+  imageSelector("1")
+})
+
+mobileImage1.addEventListener("click", function(){
+  imageSelector("1")
+})
+
+desktopImage2.addEventListener("click", function(){
+  imageSelector("2")
+})
+
+mobileImage2.addEventListener("click", function(){
+  imageSelector("2")
+})
+
+desktopImage3.addEventListener("click", function(){
+  imageSelector("3")
+})
+
+mobileImage3.addEventListener("click", function(){
+  imageSelector("3")
+})
+
+function imageSelector(data){
   if (viewSwitch == false) {
+    largeImage.src = "img/example-" + data + ".jpg"
     overlay.style.display = "flex"
     body.style.overflowY = "hidden"
     viewSwitch = true
   }
-})
+}
 
 overlay.addEventListener("click", function(){
   if (viewSwitch == true) {
     overlay.style.display = "none"
+    body.style.overflowY = "scroll"
     viewSwitch = false
   }
 })
--- js/pop-up-menu.js
+++ js/pop-up-menu.js
@@ -1,3 +1,4 @@
+
 const popUpMenu = document.querySelector(".mobile-pop-up-menu")
 const menuList = document.querySelector(".link-list-mobile")
 const menuSocials = document.querySelector(".socials")
@@ -5,23 +6,13 @@ const menuButton = document.querySelector(".mobile-burger")
 const page = document.querySelector(".page-container")
 let buttonSwitch = false
 
-window.addEventListener("resize", function(){
-  let windowWidth = window.innerWidth
-  if (buttonSwitch == true){
-    if (windowWidth >= 992){
-      popUpMenu.style.animation = "pop-down-menu 0.15s forwards"
-      setTimeout(function(){
-        popUpMenu.style.display = "none"
-      }, 150)
-      buttonSwitch = false
-    }
-  }
-})
 
+// On menu button click this function uses an if statement to decide whether to open or close the menu.
 menuButton.addEventListener("click", function(){
   if (buttonSwitch == false){
     popUpMenu.style.display = "flex"
     popUpMenu.style.animation = "pop-up-menu 0.3s forwards"
+    // I used the setTimeout function to wait for my keyframes to complete before continuing.
     setTimeout(function(){
       buttonSwitch = true
     }, 300)
@@ -34,12 +25,26 @@ menuButton.addEventListener("click", function(){
   }
 })
 
+// While the menu is open, if anything other than the menu is clicked this function will close the menu.
 page.addEventListener("click", function(){
   if (buttonSwitch == true){
     popUpMenu.style.animation = "pop-down-menu 0.3s forwards"
     setTimeout(function(){
       popUpMenu.style.display = "none"
-      buttonSwitch = false
     }, 300)
+  }
+})
+
+// Monitors screen size and closes the menu if the page becomes too wide.
+window.addEventListener("resize", function(){
+  let windowWidth = window.innerWidth
+  if (buttonSwitch == true){
+    if (windowWidth >= 992){
+      popUpMenu.style.animation = "pop-down-menu 0.15s forwards"
+      setTimeout(function(){
+        popUpMenu.style.display = "none"
+      }, 150)
+      buttonSwitch = false
+    }
   }
 })
